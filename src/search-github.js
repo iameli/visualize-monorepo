eb039b68305c0292c48657d1661bb958328ae269
Eli Mallon <eli@livepeer.org>
2019-02-24 21:01:21 -0800

    first attempt at cache

--- src/search-github.js
+++ src/search-github.js
@@ -5,16 +5,36 @@ import { btoa, atob } from "isomorphic-base64";
 const githubFetch = fileUrl =>
   fetch(`https://api.github.com/repos/${fileUrl}`, {
     headers: {
+      "user-agent": "monorepo.ooo",
       authorization: "Basic " + btoa(user + ":" + token)
     }
   });
 
-export default async function searchGithub(repo) {
+const toJson = async res => {
+  const text = await res.text();
+  try {
+    return JSON.parse(text);
+  } catch (e) {
+    throw new Error(`failed to parse JSON: ${text}`);
+  }
+};
+
+export async function getCommitGithub(repo) {
+  console.log(repo);
+  let res = await githubFetch(`${repo}`);
+  const { default_branch } = await toJson(res);
+  res = await githubFetch(`${repo}/branches`);
+  const branches = await toJson(res);
+  const branch = branches.find(branch => branch.name === default_branch);
+  return branch.commit.sha;
+}
+
+export async function searchGithub(repo, commit) {
   let res = await githubFetch(`${repo}/contents/package.json`);
   if (!res.ok) {
     throw new Error(`${res.status} ${await res.text()}`);
   }
-  const { content } = await res.json();
+  const { content } = await toJson(res);
   const rootPkg = JSON.parse(atob(content));
   const workspaces = rootPkg.workspaces;
   const pkgPaths = [];
@@ -29,13 +49,13 @@ export default async function searchGithub(repo) {
     // If it does, we gotta search for them all
     const prefix = wPath.slice(0, wPath.length - 2);
     res = await githubFetch(`${repo}/contents/${prefix}`);
-    const dirs = await res.json();
+    const dirs = await toJson(res);
     pkgPaths.push(...dirs.map(dir => dir.path));
   }
   const output = {};
   for (const pkgPath of pkgPaths) {
     res = await githubFetch(`${repo}/contents/${pkgPath}/package.json`);
-    const { content } = await res.json();
+    const { content } = await toJson(res);
     const pkg = JSON.parse(atob(content));
     output[pkg.name] = pkg;
   }
--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,5 @@
 import render from "./render";
-import searchGithub from "./search-github";
+import { searchGithub, getCommitGithub } from "./search-github";
 import makeDot from "./make-dot";
 
 if (typeof self.skipWaiting === "function") {
@@ -20,7 +20,7 @@ async function fetchAndApply(req) {
     if (!req.url.includes("svg")) {
       return fetch(req);
     }
-    const { pathname } = new URL(req.url);
+    const { pathname, origin } = new URL(req.url);
     if (!pathname.startsWith(GITHUB_PREFIX)) {
       throw new Error("only github supported for now");
     }
@@ -28,10 +28,25 @@ async function fetchAndApply(req) {
       GITHUB_PREFIX.length,
       pathname.length - SVG_SUFFIX.length
     );
-    console.log(repo);
-    const pkgs = await searchGithub(repo);
+    // One slash means no SHA
+    if (repo.split("").filter(x => x === "/").length < 2) {
+      const commit = await getCommitGithub(repo);
+      return Response.redirect(
+        `${origin}/${GITHUB_PREFIX}${repo}/${commit}.svg`
+      );
+    }
+    const pkgs = await searchGithub(
+      repo
+        .split("/")
+        .slice(0, 2)
+        .join("/")
+    );
     const dot = makeDot(pkgs);
-    return new Response(await render(dot));
+    return new Response(await render(dot), {
+      headers: {
+        "Cache-Control": "max-age=31556926"
+      }
+    });
   } catch (err) {
     return new Response([err.message, err.stack].join("\n"));
   }
